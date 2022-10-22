97d037f152ecc329393464f7cecbc22a2e1d3ec8
praneethreddyk <72781865+praneethreddyk@users.noreply.github.com>
2022-10-21 23:16:49 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 import java.io.IOException;
 
 
-public class App{
+public class Trading{
 	
 	
-	public App() throws IOException{
+	public Trading() throws IOException{
 		boolean loginStatus;
 		Facade facade = new Facade();
 		loginStatus = facade.login();
@@ -16,7 +16,7 @@ public class App{
 
 	public static void main(String[] args) throws IOException{
 		System.out.println("Hello have a good time in trading");
-		new App();
+		new Trading();
 	}
 
 }
